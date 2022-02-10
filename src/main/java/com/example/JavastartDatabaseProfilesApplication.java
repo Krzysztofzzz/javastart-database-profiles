79235260a6af3bf8b716a0683ed15d68528bde61
Krzysztof <krzubrycki@gmail.com>
2022-02-10 17:08:43 +0100

    testing profiles

--- src/main/java/com/example/JavastartDatabaseProfilesApplication.java
+++ src/main/java/com/example/JavastartDatabaseProfilesApplication.java
@@ -2,12 +2,15 @@ package com.example;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication
 public class JavastartDatabaseProfilesApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(JavastartDatabaseProfilesApplication.class, args);
+        ConfigurableApplicationContext context = SpringApplication.run(JavastartDatabaseProfilesApplication.class, args);
+        ProductRepository productRepository = context.getBean(ProductRepository.class);
+        productRepository.findAll().forEach(System.out::println);
     }
 
 }
